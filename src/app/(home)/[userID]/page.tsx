f7cfb3b773105491d4afb597a654cb9687be200d
Felix <fcchungg@gmail.com>
2023-06-25 10:49:18 -0700

    press enter to send message

--- src/app/(home)/[userID]/page.tsx
+++ src/app/(home)/[userID]/page.tsx
@@ -19,21 +19,19 @@ import {
   Flex,
   Container,
   Divider,
-  CircularProgress
+  CircularProgress,
 } from "@chakra-ui/react";
 import { handleClientScriptLoad } from "next/script";
 import MessageBubble from "@/components/message_bubble";
 import { useEffect, useRef, useState } from "react";
 import MessagesContainer from "@/components/messages_list";
 import TextBox from "@/components/textbox";
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from "next/navigation";
 import useSWR from "swr";
 
 export default function ChatBotPage() {
- 
-  const query = useSearchParams()
-  const userID = query.get('userID')
-
+  const query = useSearchParams();
+  const userID = query.get("userID");
 
   //fetch star information
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
@@ -61,17 +59,20 @@ export default function ChatBotPage() {
       fetch("/api/chat", {
         method: "POST",
         body: JSON.stringify({
-          question: message.content = userInput,
-          history: conversation
+          question: (message.content = userInput),
+          history: conversation,
         }),
         headers: {
           "Content-type": "application/json",
         },
-      }).then((response) => response.json()).then((data) => {
-        setBotRes(data.answer);
-      }).catch((error) => {
-        console.log(error);
-      });
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setBotRes(data.answer);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
       // setBotRes(res);
     }
     //how to make seperate user message vs chatbot message?
@@ -83,61 +84,69 @@ export default function ChatBotPage() {
       setBotRes("");
     }
   };
-  
+
   // if (error) return <div>Failed to load</div>
-//    if (!data) return  <Flex
-//    width={"100vw"}
-//    height={"100vh"}
-//    alignContent={"center"}
-//    justifyContent={"center"}
-//  ><Center><CircularProgress isIndeterminate color="#2C2ABB" /></Center></Flex>
+  //    if (!data) return  <Flex
+  //    width={"100vw"}
+  //    height={"100vh"}
+  //    alignContent={"center"}
+  //    justifyContent={"center"}
+  //  ><Center><CircularProgress isIndeterminate color="#2C2ABB" /></Center></Flex>
 
   // user typing in the message
   const handleMessageChange = (event) => {
     setUserInput(event.target.value);
   };
 
-   // scroll to bottom on new message recieve/sent with use effect
-   const chatbox = useRef(null);
-   useEffect(() => {
-     const chatboxElement = chatbox.current;
-      document.title = 'Chat'
-     chatboxElement.scrollIntoView({ behavior: "smooth" });
-     chatboxElement.scrollTop = chatboxElement.scrollHeight;
-     
-     
-   }, [conversation]);
- 
+  // scroll to bottom on new message recieve/sent with use effect
+  const chatbox = useRef(null);
+  useEffect(() => {
+    const chatboxElement = chatbox.current;
+    document.title = "Chat";
+    chatboxElement.scrollIntoView({ behavior: "smooth" });
+    chatboxElement.scrollTop = chatboxElement.scrollHeight;
+  }, [conversation]);
+
+  //press enter to submit
+  const handleKeypress = (event) => {
+    if (event.keyCode === 13) {
+      userInputOnclick();
+    }
+  };
+
   return (
-   
-      <Grid paddingTop={"10vh"} paddingInline={60}>
-      
+    <Grid paddingTop={"10vh"} paddingInline={60}>
       <section id="header">
-      <Flex className="py-4">
-        <Avatar src="https://bit.ly/sage-adebayo" />
-        <Box ml="3">
-          <Text fontWeight="bold">
-            Segun Adebayo
-            <Badge ml="1" colorScheme="yellow">
-              Responding
-            </Badge>
-          </Text>
-          <Text fontSize="sm">Last seen two hours ago</Text>
-        </Box>
-      </Flex>
-      <Divider></Divider>
-    
-    </section>
-    <section>
-    <MessagesContainer conversation={conversation} chatBoxRef={chatbox}></MessagesContainer>
-    </section>
-    <section className="relative bottom-0 w-full py-10" >
+        <Flex className="py-4">
+          <Avatar src="https://bit.ly/sage-adebayo" />
+          <Box ml="3">
+            <Text fontWeight="bold">
+              Segun Adebayo
+              <Badge ml="1" colorScheme="yellow">
+                Responding
+              </Badge>
+            </Text>
+            <Text fontSize="sm">Last seen two hours ago</Text>
+          </Box>
+        </Flex>
+        <Divider></Divider>
+      </section>
+      <section>
+        <MessagesContainer
+          conversation={conversation}
+          chatBoxRef={chatbox}
+        ></MessagesContainer>
+      </section>
+      <section className="relative bottom-0 w-full py-10">
         <Center>
-          <TextBox handleClick={userInputOnclick} handleMessageChange={handleMessageChange} userInput={userInput}></TextBox>
+          <TextBox
+            handleClick={userInputOnclick}
+            handleMessageChange={handleMessageChange}
+            userInput={userInput}
+            handleKeyPress={handleKeypress}
+          ></TextBox>
         </Center>
-    </section></Grid>
-   
-     
-  
+      </section>
+    </Grid>
   );
 }
--- src/components/textbox.tsx
+++ src/components/textbox.tsx
@@ -1,19 +1,34 @@
-import { InputGroup, InputRightElement, Button, Textarea } from "@chakra-ui/react"
-import { Input } from "postcss"
+import {
+  InputGroup,
+  InputRightElement,
+  Button,
+  Textarea,
+} from "@chakra-ui/react";
+import { Input } from "postcss";
 
-const TextBox = (props: {handleClick: any, handleMessageChange: any, userInput: any}) => {
-    const {handleClick, handleMessageChange, userInput} = props
-    return (
-        <InputGroup size='md'>
-      <Textarea resize={"none"} placeholder="Here is a sample placeholder" onChange={handleMessageChange} value={userInput} />
-      <InputRightElement width='5.5rem' >
-        <Button h='1.75rem' size='sm' onClick={handleClick}>
+const TextBox = (props: {
+  handleKeyPress: any;
+  handleClick: any;
+  handleMessageChange: any;
+  userInput: any;
+}) => {
+  const { handleKeyPress, handleClick, handleMessageChange, userInput } = props;
+  return (
+    <InputGroup size="md">
+      <Textarea
+        resize={"none"}
+        placeholder="Here is a sample placeholder"
+        onChange={handleMessageChange}
+        value={userInput}
+        onKeyDown={handleKeyPress}
+      />
+      <InputRightElement width="5.5rem">
+        <Button h="1.75rem" size="sm" onClick={handleClick}>
           Send
         </Button>
       </InputRightElement>
     </InputGroup>
-    )
-}
+  );
+};
 
 export default TextBox;
-
